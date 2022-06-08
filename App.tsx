f5fccdfbf5409fc6b00b1f2ea859ebbbf9158942
Alecsander Marques <alecsander_marques@hotmail.com>
2022-06-08 08:21:39 -0300

    added react-native-vector-icons

--- App.tsx
+++ App.tsx
@@ -1,6 +1,7 @@
-import {Box, NativeBaseProvider, StatusBar} from 'native-base';
+import {Box, NativeBaseProvider, StatusBar, Text} from 'native-base';
 import React from 'react';
 import {SafeAreaView, useColorScheme} from 'react-native';
+import Icon from 'react-native-vector-icons/Ionicons';
 
 export default function App() {
   const isDarkMode = useColorScheme() === 'dark';
@@ -10,6 +11,10 @@ export default function App() {
         <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
 
         <Box>Hello world</Box>
+        <Icon name="ios-person" size={30} color="#4F8EF7" />
+        <Text>
+          Lorem <Icon name="ios-book" color="#4F8EF7" /> Ipsum
+        </Text>
       </SafeAreaView>
     </NativeBaseProvider>
   );
